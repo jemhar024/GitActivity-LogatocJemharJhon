914a9562cb49e03f3d0331a86968a3e804b2b6ba
LogatocJemharJhon <jemharlandoylogatoc@gmail.com>
2022-03-13 01:41:36 +0800

    My Educational Background added

--- git_python_logatoc.py
+++ git_python_logatoc.py
@@ -1,1 +1,7 @@
+print("Jemhar Jhon L. Logatoc")
+print("Mahinhin Boac Marinduque")
+print("November 24 2002")
+print("college of engineering information technology and industrial technology")
+print("bachelor of science in information technology")
+print("BSIT2D")
 
